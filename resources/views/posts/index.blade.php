b7ecf9ea486a7043e1ed89fd893cfd06a412ce1d
takekura <takeru20011115@gmail.com>
2024-01-05 09:36:41 +0900

    show画面制作

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,7 +9,9 @@
      <h1>Blog Name</h1>
      <div class = "posts">
        @foreach ($posts as $post)
-         <h2 class='title'>{{ $post->title }}</h2>
+         <h2 class='title'>
+            <a href = "/posts/{{$post->id}}">{{$post->title}}</a> 
+         </h2>
          <p class='body'>{{ $post->body }}</p>
        @endforeach
      </div>

1641b41319040892d9741ac1ace012f2b593ffbd
takekura <takeru20011115@gmail.com>
2024-01-13 02:11:40 +0900

    edit画面完成

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -18,6 +18,8 @@
             </div>
         </div>
         <div class="footer">
+            <div class = "edit"><a href = "/posts/{{ $post->id}}/edit">Edit</a></div>
+            
             <a href="/">戻る</a>
         </div>
     </body>
